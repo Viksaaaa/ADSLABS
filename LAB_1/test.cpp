dea3502c4a03da48855a3884a741dc8ceb1c33a4
Viksaaaa <86292596+Viksaaaa@users.noreply.github.com>
2021-11-15 17:00:47 +0300

    feat: add insert method tests

--- LAB_1/test.cpp
+++ LAB_1/test.cpp
@@ -583,13 +583,13 @@ TEST(insert, kjbsof)
 	}
 	ASSERT_EQ( list2.get_size(), start_size_list2 );
 
-	size_t end_size{ start_size_list1 + list2.get_size() };
+	size_t end_size{ start_size_list1 + start_size_list2 };
 	size_t position{};
 
 	list1.insert( position, list2 );
 
 	ASSERT_EQ( list1.get_size(), end_size );
-	ASSERT_EQ( list1.at(position), list2.at(0) );
+	ASSERT_EQ( list1.at(position), list2.at(position) );
 	
 	end_size = list1.get_size() + end_size ;
 	position = 13;
