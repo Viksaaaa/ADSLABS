b0dbf0260f71afbec6467bda097ebfaee6285dd4
Viksaaaa <86292596+Viksaaaa@users.noreply.github.com>
2022-05-31 12:21:48 +0300

    feat: add test

--- Laba2/test.cpp
+++ Laba2/test.cpp
@@ -1,6 +1,10 @@
+#include "shannon_fano.h"
 
-
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+int main()
+{
+	setlocale(LC_ALL, "");
+	shannon_fano device{};
+	device.encode("Если человек не потерял способности ждать счастья — он счастлив. Это и есть счастье.");
+	device.info();
+	return 0;
 }
